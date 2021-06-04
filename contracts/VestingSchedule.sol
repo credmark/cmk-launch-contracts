34533664b4c1d3183af9a3a9489fbfb67726594b
Neil Zumwalde <neilzumwalde@gmail.com>
2021-06-04 12:03:15 -0400

    added zokyo's feedback

--- contracts/VestingSchedule.sol
+++ contracts/VestingSchedule.sol
@@ -1,14 +1,11 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract VestingSchedule is Context, AccessControl {
-
-    using SafeMath for uint;
 
     bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
 
@@ -38,11 +35,11 @@ contract VestingSchedule is Context, AccessControl {
 
     // FUNCTIONS
 
-    function addVestingSchedule(address account, uint allocation, uint vestingSeconds, uint cliffSeconds) public onlyOwner {
+    function addVestingSchedule(address account, uint allocation, uint vestingSeconds, uint cliffSeconds) external onlyOwner {
 
         require(_vestingSchedules[account].account==address(0x0), "ERROR: Vesting Schedule already exists" );
         require(cliffSeconds <= vestingSeconds, "ERROR: Cliff longer than Vesting Time");
-        require(_totalAllocation.add(allocation) <= _token.balanceOf(address(this)), "ERROR: Total allocation cannot be greater than reserves");
+        require(_totalAllocation + allocation <= _token.balanceOf(address(this)), "ERROR: Total allocation cannot be greater than reserves");
         require(vestingSeconds > 0, "ERROR: Vesting Time cannot be 0 seconds");
 
         _totalAllocation += allocation;
@@ -57,7 +54,7 @@ contract VestingSchedule is Context, AccessControl {
         emit VestingScheduleAdded(account, allocation, block.timestamp, vestingSeconds, cliffSeconds);
     }
     
-    function claim() public {
+    function claim() external  {
         return _claim(_msgSender());
     }
 
@@ -72,11 +69,11 @@ contract VestingSchedule is Context, AccessControl {
         emit AllocationClaimed(account, amount, block.timestamp);
     }
 
-    function cancel(address account) public onlyOwner {
+    function cancel(address account) external onlyOwner {
 
         uint unvestedAllocation = getUnvestedAmount(account);
 
-        _vestingSchedules[account].allocation = _vestingSchedules[account].allocation.sub(unvestedAllocation);
+        _vestingSchedules[account].allocation = _vestingSchedules[account].allocation - unvestedAllocation;
         _vestingSchedules[account].vestingSeconds = getElapsedVestingTime(account);
 
         _totalAllocation -= unvestedAllocation;
@@ -87,50 +84,50 @@ contract VestingSchedule is Context, AccessControl {
     // GETTERS
 
     ///// global /////
-    function getTokenAddress() public view returns (address) {
+    function getTokenAddress() external view returns (address) {
         return address(_token);
     }
 
-    function getTotalAllocation() public view returns (uint) {
+    function getTotalAllocation() external view returns (uint) {
         return _totalAllocation;
     }
 
-    function getTotalClaimedAllocation() public view returns (uint) {
+    function getTotalClaimedAllocation() external view returns (uint) {
         return _totalClaimedAllocation;
     }
 
     ///// by vesting definition /////
-    function getVestingSchedule(address account) public view returns (VestingScheduleStruct memory) {
+    function getVestingSchedule(address account) external view returns (VestingScheduleStruct memory) {
         return _vestingSchedules[account];
     }
 
     function getVestingMaturationTimestamp(address account) public view returns (uint) {
-        return _vestingSchedules[account].startTimestamp.add(_vestingSchedules[account].vestingSeconds);
+        return _vestingSchedules[account].startTimestamp + _vestingSchedules[account].vestingSeconds;
     }
 
     function getElapsedVestingTime(address account) public view returns (uint) {
         if(block.timestamp > getVestingMaturationTimestamp(account)){
             return _vestingSchedules[account].vestingSeconds;
         }
-        return block.timestamp.sub(_vestingSchedules[account].startTimestamp);
+        return block.timestamp - _vestingSchedules[account].startTimestamp;
     }
 
     function getVestedAmount(address account) public view returns (uint) {
-        return _vestingSchedules[account].allocation.mul( getElapsedVestingTime(account) ).div(_vestingSchedules[account].vestingSeconds);
+        return _vestingSchedules[account].allocation * getElapsedVestingTime(account) / _vestingSchedules[account].vestingSeconds;
     }
 
     function getUnvestedAmount(address account) public view returns (uint) {
-        return _vestingSchedules[account].allocation.sub(getVestedAmount(account));
+        return _vestingSchedules[account].allocation - getVestedAmount(account);
     }
 
     function getClaimableAmount(address account) public view returns (uint) {
         //If it's earlier than the cliff, zero allocation is claimable.
-        if(block.timestamp < _vestingSchedules[account].startTimestamp.add(_vestingSchedules[account].cliffSeconds)){
+        if(block.timestamp < (_vestingSchedules[account].startTimestamp + _vestingSchedules[account].cliffSeconds ) ){
             return 0;
         }
 
         //Claimable amount is the vested, unclaimed amount.
-        return getVestedAmount(account).sub(_vestingSchedules[account].claimedAmount);
+        return getVestedAmount(account) - _vestingSchedules[account].claimedAmount;
     }
 
     // MODIFIERS
